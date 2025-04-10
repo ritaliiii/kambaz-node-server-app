a007147c851cae478407e7bdb6db4c411201790f
cyanling2 <ling.24@foxmail.com>
2025-04-09 22:42:17 -0700

    asfds

--- Kambaz/Users/dao.js
+++ Kambaz/Users/dao.js
@@ -5,7 +5,13 @@ export const createUser = (user) => {
 export const findAllUsers = () => model.find();
 export const findUserById = (userId) => model.findById(userId);
 export const findUserByUsername = (username) => model.findOne({userame:username});
-export const findUserByCredentials = (username, password) => model.findOne({username, password});
+export const findUserByCredentials = (username, password) => {
+  console.log(username, password);
+  console.log(model);
+  const res = model.findOne({username, password});
+  console.log(res);
+  return res;
+}
 export const updateUser = (userId, user) => model.updateOne({_id: userId}, {$set:user});
 export const deleteUser = (userId) => model.deleteOne({_id: userId});
 export const findUsersByRole = (role) => model.find({ role: role }); 
