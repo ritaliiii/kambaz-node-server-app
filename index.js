aef5711e9cd290f79c17431cb7bb8f6d0d070ab0
cyanling2 <ling.24@foxmail.com>
2025-04-09 23:06:47 -0700

    asdsd

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ import mongoose from 'mongoose';
 
 const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kambaz"
 mongoose.connect(CONNECTION_STRING);
+console.log("conection: ", CONNECTION_STRING);
 const app = express()
 app.use(cors(
     {
